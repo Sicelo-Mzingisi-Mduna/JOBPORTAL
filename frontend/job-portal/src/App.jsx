ead9368ae0e20455a7a30263794298eee9f4371a
Sicelo Mduna <siceloit@gmail.com>
2025-10-23 21:32:31 +0200

    Created Project structure

--- frontend/job-portal/src/App.jsx
+++ frontend/job-portal/src/App.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast';
+import LandingPage from './components/LandingPage';
+import SignUp from './pages/Auth/SignUp';
+import Login from './pages/Auth/Login';
+import JobSeekerDashboard from './pages/JobSeeker/JobSeekerDashboard';
+import JobDetails from './pages/JobSeeker/JobDetails';
+import SavedJobs from './pages/JobSeeker/SavedJobs';
+import UserProfile from './pages/JobSeeker/UserProfile';
+import EmployerDashboard from './pages/Employer/EmployerDashboard';
+import JobPostingForm from './pages/Employer/JobPostingForm';
+import ManageJobs from './pages/Employer/ManageJobs';
+import ApplicationViewer from './pages/Employer/ApplicationViewer';
+import EmployerProfilePage from './pages/Employer/EmployerProfilePage';
+import ProtectedRoute from './components/ProtectedRoute';
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path= "/" element = {<LandingPage />} />
+          <Route path= "/signup" element = {<SignUp />} />
+          <Route path= "/login" element = {<Login />} />
+
+          <Route path= "/find-jobs" element = {<JobSeekerDashboard/>} />
+          <Route path= "/job/:jobId" element = {<JobDetails />} />
+          <Route path= "/saved-jobs" element = {<SavedJobs />} />
+          <Route path= "/profile" element = {<UserProfile />} />
+
+          {/* Protected */}
+          <Route element = {<ProtectedRoute requiredRole = "employer"/>}>
+            <Route path= "/employer/dashboard" element = {<EmployerDashboard />} />
+            <Route path= "/post-job" element = {<JobPostingForm />} />
+            <Route path= "/manage-jobs" element = {<ManageJobs />} />
+            <Route path= "/applicants" element = {<ApplicationViewer />} />
+            <Route path= "/company-profile" element = {<EmployerProfilePage />} />
+            
+          </Route>
+
+          {/* Catch all routes */}
+          <Route path="*" element={<Navigate to = "/" replace />} />
+        </Routes>
+      </Router>
+      
+    </div>
   )
 }
 
